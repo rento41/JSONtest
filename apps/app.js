bd888115e96adc1db11349f1848834e1f3c8c385
rento41 <adnan.nametak@gmail.com>
2015-10-09 15:25:27 +0200

    Api Hack

--- apps/app.js
+++ apps/app.js
@@ -1,29 +1,43 @@
-$(function(){
-	$('#search-term').submit(function(event){
-		event.preventDefault();
-		var searchTerm = $('#query').val();
-		getRequest(searchTerm);
-	});
-})
+// $(function(){
+// 	$('#search-term').submit(function(event){
+// 		event.preventDefault();
+// 		var searchTerm = $('#query').val();
+// 		getRequest(searchTerm);
+// 	});
+// })
 
-function getRequest(searchTerm){
-	var params = {
-		s: searchTerm,
-		r: 'json'
-	};
-	url = 'http://www.omdbapi.com';
-	
-	$.getJSON(url,params, function(data){
-		showResults(data.Search);
-	});
-}
+$(document).ready(function() {
 
-function showResults(results){
-	var html = "";
-	$.each(results, function(index, value){
-		html += '<p>' + value.Title + '</p>';
-		// console.log(value.Title);
-	});
-	$('#search-results').html(html);
-}
+    $('#etsySearch').submit(function(event){
+        event.preventDefault();
+        var searchTerm = $('#query').val();
+        var searchNumber = $('#query2').val();
+        getRequest(searchTerm,searchNumber);
+    });
+    
+    function getRequest(searchTerm,searchNumber){
+       
+        $.ajax({
+            dataType: 'jsonp',
+            kolicina: searchNumber,
+            url : 'https://openapi.etsy.com/v2/listings/active.js?keywords=' + searchTerm + '&limit=' + searchNumber + '&includes=Images:1&api_key=gm8hkqlwavvup5paouv4vdu5',
+            success: function(data){
+                showResults(data);
+            }
+       });
+    }
+    
+    function showResults(data){
+        var value = "";
+        $.each(data.results, function(index, item){  
+            console.log(data);
+            if(index == 0){
+                $('#slajder').html('<div class="item active"><a href="' + item.url + '"><img src="' + item.Images[0].url_fullxfull + '" clas="img-responsive"/></a></div>')
+            }
+            else{
+                $('#slajder').append('<div class="item"><a href="' + item.url + '"><img src="' + item.Images[0].url_fullxfull + '" clas="img-responsive"/></a></div>')
+            }
+        });
+    }
+});
 
